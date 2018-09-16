87d3ef70225635cee0f844ef8163a92f6f5613c9
Alex Baumgertner <alex.baumgertner@gmail.com>
2018-09-16 23:51:05 +0300

    Обновляет gatsby до версии 2 beta (#8)
    * Обновляет gatsby до версии 2 beta
    
    Fix #7
    
    На основе
    - https://github.com/gatsbyjs/gatsby-starter-default#v2
    - https://next.gatsbyjs.org/docs/gatsby-starters#using-gatsby-v2-with-the-official-starters
    - https://github.com/gatsbyjs/gatsby-starter-default/blob/v2/package.json
    
    Сделать каноничный реэкспорт:
    http://jamesknelson.com/re-exporting-es6-modules/

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import {
+    StaticQuery,
+    graphql
+} from 'gatsby';
+
+import {Header} from '../../components';
+import './Layout.css';
+
+const Layout = ({children}) => (
+    <StaticQuery
+        query={graphql`
+            query SiteTitleQuery {
+                site {
+                  siteMetadata {
+                        title,
+                        description,
+                        keywords
+                    }
+                }
+          }
+        `}
+        render={data => (
+            <>
+                <Helmet
+                    title={data.site.siteMetadata.title}
+                    meta={[
+                        {
+                            name: 'description',
+                            content: data.site.siteMetadata.description
+                        },
+                        {
+                            name: 'keywords',
+                            content: data.site.siteMetadata.keywords
+                        }
+                    ]}
+                >
+                    <html lang="en"/>
+                </Helmet>
+                <Header siteTitle={data.site.siteMetadata.title}/>
+                <main>
+                    {children}
+                </main>
+            </>
+        )}
+    />
+);
+
+Layout.propTypes = {
+    children: PropTypes.node.isRequired
+};
+
+export default Layout;
