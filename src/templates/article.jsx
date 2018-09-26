f5ef9c9a38a55a2fde8ac996f83aba82aed8d3e4
alexbaumgertner <alex.baumgertner@gmail.com>
2018-09-27 00:27:00 +0400

    Поправляет замечания eslint

--- src/templates/article.jsx
+++ src/templates/article.jsx
@@ -2,12 +2,9 @@ import React from 'react';
 import {graphql} from 'gatsby';
 
 export default (props) => {
-    console.log('props: ', props); // eslint-disable-line
-
     const {
         data: {
             markdownRemark: {
-                excerpt,
                 frontmatter,
                 html
             }
