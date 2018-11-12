a6bf5bdf9a489b09171c2cf3a30a90fb6942bf9e
Mark Langovoi <langovoi@users.noreply.github.com>
2018-11-13 00:09:02 +0300

    Страница событий (#22)
    Добавил страницу предстоящих событий из [календаря](https://web-standards.ru/calendar.ics)
    
    Для получения данных для страницы написал плагин [gatsby-source-ical](https://github.com/langovoi/gatsby-source-ical), который позволяет загружать `iCalendar` файлы и дает доступ к их содержимому через GraphQL API.
    
    Так же добавил `graphql.config.json`, который позволяет интегрироваться с различными IDE для удобной работы с GraphQL API.

---
+++ src/pages/events.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+    StaticQuery,
+    graphql
+} from 'gatsby';
+import {Layout} from "../components/Layout";
+
+
+export default () => {
+    const currentDate = new Date();
+
+    return (
+        <Layout>
+            <StaticQuery
+                query={graphql`
+                query EventsQuery {
+                    allIcal(filter: {sourceInstanceName: {eq: "events"}} sort: {order: ASC fields: [start]}) {
+                        edges {
+                            node {
+                                start
+                                end
+                                summary
+                                description
+                                location
+                            }
+                        }
+                    }
+                }
+            `}
+                render={({allIcal: {edges: events}}) =>
+                    <ul>
+                        {events
+                            .reduce((events, event) => {
+                                event = event.node;
+                                event.dtstamp = new Date(event.dtstamp);
+                                event.start = new Date(event.start);
+                                event.end = new Date(event.end);
+
+                                if (event.end < currentDate) {
+                                    return events
+                                }
+
+                                return [
+                                    ...events,
+                                    event
+                                ]
+                            }, [])
+                            .map(event => (
+                                <li>
+                                    <b>{event.summary}</b><br/>
+                                    <p>
+                                        Описание: {event.description}<br/>
+                                        Место: {event.location}<br/>
+                                        Начало: {event.start.toLocaleString()}<br/>
+                                        Конец: {event.end.toLocaleString()}<br/>
+                                    </p>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                }
+            />
+        </Layout>
+    );
+};
