3fb85a3623953336d5b90a7a578f8365113e859e
Vadim Makeev <pepelsbey@gmail.com>
2018-12-12 01:00:24 +0300

    Размечает ссылки списком

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,16 +2,31 @@ import React from 'react';
 
 const Footer = () => (
     <footer>
+        <h3>Соцсети</h3>
+        <ul>
+            <li>
+                <a href="https://twitter.com/webstandards_ru">Твиттер</a>
+            </li>
+            <li>
+                <a href="https://vk.com/webstandards_ru">ВКонтакте</a>
+            </li>
+            <li>
+                <a href="https://github.com/web-standards-ru">Гитхаб</a>
+            </li>
+            <li>
+                <a href="https://fb.com/webstandardsru">Фейсбук</a>
+            </li>
+            <li>
+                <a href="https://www.youtube.com/webstandards_ru">Ютуб</a>
+            </li>
+            <li>
+                <a href="http://slack.web-standards.ru">Слак</a>
+            </li>
+            <li>
+                <a href="https://telegram.me/webstandards_ru">Телеграм</a>
+            </li>
+        </ul>
         <p>2018 © Веб-стандарты</p>
-        <p>
-            <a href="https://twitter.com/webstandards_ru">Твиттер</a>
-            <a href="https://vk.com/webstandards_ru">ВКонтакте</a>
-            <a href="https://github.com/web-standards-ru">Гитхаб</a>
-            <a href="https://fb.com/webstandardsru">Фейсбук</a>
-            <a href="https://www.youtube.com/webstandards_ru">Ютуб</a>
-            <a href="http://slack.web-standards.ru">Слак</a>
-            <a href="https://telegram.me/webstandards_ru">Телеграм</a>
-        </p>
     </footer>
 );
 
