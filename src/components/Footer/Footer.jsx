a41c4cbab0ab420b61529c88f3bf03da5453ce7d
Vadim Makeev <pepelsbey@gmail.com>
2018-12-11 17:16:00 +0300

    Склеивает теги обратно, упрощает разметку

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,52 +1,17 @@
 import React from 'react';
 
 const Footer = () => (
-    <footer className="footer">
+    <footer>
         <p>2018 © Веб-стандарты</p>
-        <div className="footer__social">
-            <a
-                className="social__link social__link--twitter"
-                href="https://twitter.com/webstandards_ru"
-            >
-                <span className="social__text">Твиттер</span>
-            </a>
-            <a
-                className="social__link social__link--vk"
-                href="https://vk.com/webstandards_ru"
-            >
-                <span className="social__text">ВКонтакте</span>
-            </a>
-            <a
-                className="social__link social__link--github"
-                href="https://github.com/web-standards-ru"
-            >
-                <span className="social__text">Гитхаб</span>
-            </a>
-            <a
-                className="social__link social__link--facebook"
-                href="https://fb.com/webstandardsru"
-            >
-                <span className="social__text">Фейсбук</span>
-            </a>
-            <a
-                className="social__link social__link--youtube"
-                href="https://www.youtube.com/webstandards_ru"
-            >
-                <span className="social__text">Ютуб</span>
-            </a>
-            <a
-                className="social__link social__link--slack"
-                href="http://slack.web-standards.ru"
-            >
-                <span className="social__text">Слак</span>
-            </a>
-            <a
-                className="social__link social__link--telegram"
-                href="https://telegram.me/webstandards_ru"
-            >
-                <span className="social__text">Телеграм</span>
-            </a>
-        </div>
+        <p>
+            <a href="https://twitter.com/webstandards_ru">Твиттер</a>
+            <a href="https://vk.com/webstandards_ru">ВКонтакте</a>
+            <a href="https://github.com/web-standards-ru">Гитхаб</a>
+            <a href="https://fb.com/webstandardsru">Фейсбук</a>
+            <a href="https://www.youtube.com/webstandards_ru">Ютуб</a>
+            <a href="http://slack.web-standards.ru">Слак</a>
+            <a href="https://telegram.me/webstandards_ru">Телеграм</a>
+        </p>
     </footer>
 );
 
